249be4411d573086e120f751450d66316d52a888
Sthabile Lushaba <sthabile@switch.tj>
2021-09-02 18:45:02 +0200

    Updated files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+//Importing components
+import Form from './Components/Form';
+import TodoList from './Components/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <header>
+      <h1>Personal Todo List</h1>
+    </header>
+    <Form />
+    <TodoList />
     </div>
   );
 }
---
+++ src/Components/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const TodoList = () => {
+    return(
+        <div clasName="todo-container">
+        <ul className="todo-list"></ul>
+      </div>
+    );
+}
+
+export default TodoList;
